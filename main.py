9148378ed3a30ca35c67203db2e01873034c82a8
saurabhjha004 <saurabhdkmjha@gmail.com>
2024-06-19 08:23:50 +0530

    code added

--- main.py
+++ main.py
@@ -4,34 +4,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.feature_extraction.text import CountVectorizer
 
+data = pd.read_csv("Youtube.csv")
+data = data[['CONTENT', 'CLASS']] # Selecting only CONTENT and CLASS columns after loading data
 
-data = pd.read_csv("Youtube.csv") 
-
-# Since we only require content and class columns, we will update our existing datafram to the following
-data = data[['CONTENT','CLASS']]
-
-# We will map 0 to not spam and 1 to span in class column
-data["CLASS"] = data['CLASS'].map({0:'NOT A SPAM COMMENT', 1: 'SPAM COMMENT'})
+# Mapping class labels
+data["CLASS"] = data['CLASS'].map({0: 'NOT A SPAM COMMENT', 1: 'SPAM COMMENT'})
 
 x = np.array(data['CONTENT'])
 y = np.array(data['CLASS'])
 
-"""
-As the output of this problem will either be 0 or 1,i.e, the problem of binary classification,
-we can use the Bernoulli Naive Bayes algorithm to train the model:
-"""
-
 cv = CountVectorizer()
 x = cv.fit_transform(x)
-xtrain, xtest, ytrain, ytest = train_test_split(x,y,train_size=0.2,random_state= 42)
+# Splitting data into train and test sets
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, train_size=0.8, random_state=42)
 
+# Bernoulli Naive Bayes model
 model = BernoulliNB()
-model.fit(xtrain,ytrain)
 
-print(model.score(xtest,ytest))
+# Training the model
+model.fit(xtrain, ytrain)
 
-# We will check the model by giving spam and non-spam comments
+# Checking the accuracy of the model on test set
+accuracy = model.score(xtest, ytest)
+print(f"Model accuracy: {accuracy}")
 
-S = input("Enter a comment : ")
-d = cv.transform([S]).toarray()
-print(model.predict(d))
+# Testing the model with user input
+user_input = input("Enter a comment: ")
+input_vector = cv.transform([user_input]).toarray()
+prediction = model.predict(input_vector)
+print(f"Prediction: {prediction}")
